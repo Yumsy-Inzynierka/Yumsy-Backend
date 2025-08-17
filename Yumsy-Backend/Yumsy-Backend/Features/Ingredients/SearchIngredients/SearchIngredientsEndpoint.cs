9b4041e1429f4b5a01deed9d14b454ff0dc81941
Mateusz Wdzienkowski <mateuszwdzienkowski@gmail.com>
2025-08-17 12:09:26 +0200

    YB-3: Removed authorization for this endpoint

--- Yumsy-Backend/Yumsy-Backend/Features/Ingredients/SearchIngredients/SearchIngredientsEndpoint.cs
+++ Yumsy-Backend/Yumsy-Backend/Features/Ingredients/SearchIngredients/SearchIngredientsEndpoint.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Yumsy_Backend.Features.Ingredients.SearchIngredient;
 
-[Authorize]
 [ApiController]
 [Route("/api/ingredients")]
 public class SearchIngredientsEndpoint : ControllerBase
