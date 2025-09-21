0e95ec80d355bf972c1ddcc1aaf846258ace3162
igorm <igormalycha@gmail.com>
2025-09-21 16:59:43 +0200

    YB: Add validation to refresh token

--- Yumsy-Backend/Yumsy-Backend/Features/Users/RefreshTokenEndpoint/RefreshTokenEndpoint.cs
+++ Yumsy-Backend/Yumsy-Backend/Features/Users/RefreshTokenEndpoint/RefreshTokenEndpoint.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Yumsy_Backend.Features.Users.RefreshTokenEndpoint;
 
@@ -7,15 +8,23 @@ namespace Yumsy_Backend.Features.Users.RefreshTokenEndpoint;
 public class RefreshTokenEndpoint : ControllerBase
 {
     private readonly RefreshTokenHandler _handler;
+    private readonly IValidator<RefreshTokenRequest> _validator;
 
-    public RefreshTokenEndpoint(RefreshTokenHandler handler)
+    public RefreshTokenEndpoint(RefreshTokenHandler handler, IValidator<RefreshTokenRequest> validator)
     {
         _handler = handler;
+        _validator = validator;
     }
 
     [HttpPost]
-    public async Task<ActionResult<RefreshTokenResponse>> Post([FromBody] RefreshTokenRequest request)
+    public async Task<ActionResult<RefreshTokenResponse>> RefreshToken([FromBody] RefreshTokenRequest request)
     {
+        var validationResult = await _validator.ValidateAsync(request);
+        if (!validationResult.IsValid)
+        {
+            throw new ValidationException(validationResult.Errors);
+        }
+        
         var result = await _handler.Handle(request);
         return Ok(result);
     }
