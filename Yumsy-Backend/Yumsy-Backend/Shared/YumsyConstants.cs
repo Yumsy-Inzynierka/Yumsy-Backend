c058640899203c17764f9a0ef3addd699460655c
Mateusz Wdzienkowski <mateusz.wdzienkowski@gmail.com>
2025-11-15 12:20:42 +0100

    Added new constant

--- Yumsy-Backend/Yumsy-Backend/Shared/YumsyConstants.cs
+++ Yumsy-Backend/Yumsy-Backend/Shared/YumsyConstants.cs
@@ -6,4 +6,6 @@ public static class YumsyConstants
     public static readonly int FETCHED_POSTS_AMOUNT = 15;
     public static readonly int TOP_DAILY_TAGS_AMOUNT = 8;
     public static readonly int TOP_DAILY_POSTS_AMOUNT = 6;
+    public static readonly int NEW_POSTS_AMOUNT = 6;
+    
 }
