d15fa1299d8d7fe541657f0da6c885a0cba46a41
Mateusz Wdzienkowski <mateusz.wdzienkowski@gmail.com>
2025-12-06 22:25:36 +0100

    YB-30: Adjusted request url for SearchPosts endpoint

--- Yumsy-Backend/Yumsy-Backend/Features/Posts/SearchPosts/SearchPostsRequest.cs
+++ Yumsy-Backend/Yumsy-Backend/Features/Posts/SearchPosts/SearchPostsRequest.cs
@@ -4,9 +4,9 @@ namespace Yumsy_Backend.Features.Posts.SearchPosts;
 
 public class SearchPostsRequest
 {
-    [FromQuery]
+    [FromQuery(Name = "query")]
     public string Query { get; init; }
 
-    [FromQuery]
+    [FromQuery(Name = "page")]
     public int Page { get; init; } = 1;
 }
