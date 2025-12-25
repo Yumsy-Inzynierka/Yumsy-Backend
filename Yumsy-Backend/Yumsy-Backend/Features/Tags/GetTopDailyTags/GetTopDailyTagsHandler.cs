8107c9b6f51a2290c8b601e5eadd082a92a3ae54
Mateusz Wdzienkowski <mateusz.wdzienkowski@gmail.com>
2025-12-25 04:28:48 +0100

    Moved logic of Top Daily Tags and Posts to database

--- Yumsy-Backend/Yumsy-Backend/Features/Tags/GetTopDailyTags/GetTopDailyTagsHandler.cs
+++ Yumsy-Backend/Yumsy-Backend/Features/Tags/GetTopDailyTags/GetTopDailyTagsHandler.cs
@@ -15,46 +15,21 @@ public class GetTopDailyTagsHandler
 
     public async Task<GetTopDailyTagsResponse> Handle(CancellationToken cancellationToken)
     {
-        var hoursSince = -24;
-        var since = DateTime.UtcNow.AddHours(hoursSince);
-        const int pool = 15;
-
-        var tagsTask = GetTags(since, cancellationToken);
-        var tags = await tagsTask;
-
-        while (tags.Count < pool)
-        {
-            hoursSince *= 2;
-            since = DateTime.UtcNow.AddHours(hoursSince);
-            tags = await GetTags(since, cancellationToken);
-
-            // Zabezpieczenie, żeby nie cofać się w nieskończoność
-            if (hoursSince < -24 * 90)
-                break;
-        }
-
-        var topTags = tags
-            .Take(YumsyConstants.TOP_DAILY_TAGS_AMOUNT)
-            .ToList();
-
+        var yesterday = DateOnly.FromDateTime(DateTime.UtcNow.AddDays(-1));
+        
+        var tags = await _dbContext.TopDailyTags
+            .Where(tdt => tdt.Date == yesterday)
+            .OrderBy(tdt => tdt.Rank)
+            .Select(tdt => new GetTopDailyTagResponse
+            {
+                Id = tdt.Tag.Id,
+                Name = tdt.Tag.Name
+            })
+            .ToListAsync(cancellationToken);
+        
         return new GetTopDailyTagsResponse
         {
-            Tags = topTags
+            Tags = tags
         };
-    }
-
-    private async Task<List<GetTopDailyTagResponse>> GetTags(DateTime since, CancellationToken cancellationToken)
-    {
-        return await _dbContext.PostTags
-            .Where(pt => pt.Post.PostedDate >= since)
-            .GroupBy(pt => new { pt.Tag.Id, pt.Tag.Name })
-            .Select(g => new GetTopDailyTagResponse
-            {
-                Id = g.Key.Id,
-                Name = g.Key.Name,
-                Count = g.Count()
-            })
-            .OrderByDescending(t => t.Count)
-            .ToListAsync(cancellationToken);
     }
 }
